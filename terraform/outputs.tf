0b0d8fdc21c1509020f7f64534568ea2c05be7c8
Narayana Yeluru <37355660+Narayana-Yeluru@users.noreply.github.com>
2026-03-25 23:12:46 -0400

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "webapp_url" {
-  value = azurerm_linux_web_app.webapp.default_hostname
+output "dotnet_app_url" {
+  value = azurerm_linux_web_app.dotnet_app.default_hostname
+}
+
+output "node_app_url" {
+  value = azurerm_linux_web_app.node_app.default_hostname
+}
+
+output "python_app_url" {
+  value = azurerm_linux_web_app.python_app.default_hostname
 }
